f4ee59799d13a8ab06d8a78d27e23acf34a125b0
fptmzmvkzm <fptmzmvkzm@gmail.com>
2024-07-24 18:34:47 +0900

    jaegyu/07.24

--- front/src/views/main/Main.js
+++ front/src/views/main/Main.js
@@ -8,14 +8,17 @@ import Cookies from 'js-cookie';
 function MainPage() {
 
     const [cookieValue, setCookieValue] = useState('');
+    
 
     const value = useContext(MainContext);
 
-    useEffect(() => {
+    useEffect((userNickname) => {
         // 쿠키 값 가져오기
         const value = Cookies.get('accessToken');
         setCookieValue(value || '쿠키가 존재하지 않습니다.');
+        
         console.log('cookie :>> ', value);
+        
       }, []);
 
     return(
