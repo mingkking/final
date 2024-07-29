94b24b069f8b51a46a2073e07136f01e2d5ac269
JUNGMINKI <jumki@naver.com>
2024-07-29 18:16:04 +0900

    20240729 minki

--- backend/SpringBoot/src/main/java/com/example/community/service/CommunityService.java
+++ backend/SpringBoot/src/main/java/com/example/community/service/CommunityService.java
@@ -1,5 +1,8 @@
 package com.example.community.service;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import com.example.community.domain.CommunityVO;
@@ -7,5 +10,9 @@ import com.example.community.domain.CommunityVO;
 @Service
 public interface CommunityService {
     public void insertCommunity(CommunityVO communityVO) throws Exception;
+
+    // 네이티브 쿼리를 사용
+    @Query(value = "SELECT * FROM COMMUNITY ORDER BY created_at DESC", nativeQuery = true)
+    public List<CommunityVO> selectAllCommunity() throws Exception;
 }
  
--- backend/SpringBoot/src/main/java/com/example/community/service/CommunityServiceImpl.java
+++ backend/SpringBoot/src/main/java/com/example/community/service/CommunityServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.community.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +15,13 @@ public class CommunityServiceImpl implements CommunityService{
     private CommunityRepository communityRepository;
 
     @Override
+    public List<CommunityVO> selectAllCommunity() throws Exception {
+        return communityRepository.selectAllCommunity();
+    }
+
+    @Override
     public void insertCommunity(CommunityVO communityVO) throws Exception {
         communityRepository.save(communityVO);
     }
-
-    
 
 }
