73f1630b2e058af1b9dcd13445a2c2c0994bdb9a
HyeGyeong Cho <judyhg.cho@gmail.com>
2024-08-13 18:14:48 +0900

    hyegyeong

---
+++ backend/SpringBoot/src/main/java/com/example/mgr/domain/MgrInterestEstateVO.java
@@ -0,0 +1,11 @@
+package com.example.mgr.domain;
+ 
+import java.util.Date; 
+
+import lombok.Data;
+ 
+@Data   
+public class MgrInterestEstateVO {
+	private String apartment_name;
+	private String user_num;
+}  
